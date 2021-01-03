884a720542f9da42d3a625fe911267bdd96cd2b2
aparna <36874105+aparna-u@users.noreply.github.com>
2021-01-03 18:30:15 +0530

    Add files via upload

---
+++ Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) => {
+    return (<div>
+         
+         <p> Name : {props.name}</p>
+         
+         <p> {props.children}</p>
+    
+    </div>
+    )}
+
+export default person;
